67e5b6e892cc5df7eb3d84e066b76bfd4f8c35a6
pablohs1986 <pabloherrero1986@gmail.com>
2020-08-25 20:22:38 +0200

    till 16.

--- ejercicios/2-JS-basics/starter/myScript.js
+++ ejercicios/2-JS-basics/starter/myScript.js
@@ -46,7 +46,7 @@ alert(firstName + " " + age + " " + job);*/
 
 // Challenge 1
 
-var heightMark = 180;
+/*var heightMark = 180;
 var heightJohn = 170;
 var massMark = 85;
 var massJohn = 80;
@@ -56,4 +56,38 @@ var bmiJohn = massJohn / (heightJohn * heightJohn);
 
 var higherBmi = bmiMark > bmiJohn;
 
-console.log("Is Mark's BMI gigher than John's? " + higherBmi);
+console.log("Is Mark's BMI gigher than John's? " + higherBmi);*/
+
+//14. IF statement
+/*var firstName = "Pablo";
+var civilStatus = "married";
+
+if(civilStatus === "married"){
+    console.log(firstName + " is married");
+} else{
+    console.log(firstName + " is not married");
+}
+
+var isMarried = false;
+
+if(isMarried){
+    console.log("married!");
+}else{
+    console.log("not married!");
+}*/
+
+//15. Boolean logic
+/*var firstName = "Pablo";
+var age = 33;
+
+if(age < 13){
+    console.log(firstName + " is a boy.")
+}else if( age >= 13 && age <= 20){
+    console.log(firstName + " is a teenager.")
+}else if( age > 20 && age <= 30){
+    console.log(firstName + " is a young man.")
+}else{
+    console.log(firstName + " is a man.")
+}*/
+
+//16 Ternary operator and Switch
