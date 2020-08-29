eeb8ff23e32ac5a2ec9aff815b4cfd03a4f9c3a4
pablohs1986 <pabloherrero1986@gmail.com>
2020-08-29 17:32:39 +0200

    til 26

--- ejercicios/2-JS-basics/myScript.js
+++ ejercicios/2-JS-basics/myScript.js
@@ -139,7 +139,7 @@ var whatDoYouDo = function(job, firstname){}*/
 
 //22. Arrays
 // Initialize
-var names = ["Sara", "Inés", "Clara"];
+/*var names = ["Sara", "Inés", "Clara"];
 var years = [33, 11, 14];
 
 console.log(names);
@@ -149,7 +149,7 @@ console.log(names.length);
 names[1] = 'Ben';
 names[5] = "Mary"
 
-console.log(names);
+console.log(names);*/
 
 // Different data types
 /*var john = ['John', 'Smith', 1988, 'teacher', false];
@@ -175,3 +175,51 @@ console.log(john.indexOf(1988)); // Returns position of the element
 var isDesigner = john.indexOf('designer') == -1 ? 'John is not a designer' : 'John is a designer'; // used for check if an element is in the array
 
 console.log(isDesigner);*/
+
+//25. Objects and properties
+/*var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    bithYear: '1986',
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false
+};
+
+// console.log(john);
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'bithYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.birthYear = 1986;
+jane['lastName'] = 'Smith';
+
+console.log(jane);*/
+
+//26. Objects and methods
+
+/*var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: '1986',
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+
+    calcAge: function(){
+        return 2020 - this.birthYear;
+    }
+};
+
+
+console.log(john.calcAge());
+
+*/
