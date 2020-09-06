39c47ce348d17630c5be4e799bc0b73415fb6a42
pablohs1986 <pabloherrero1986@gmail.com>
2020-09-06 19:36:51 +0200

    Til 50

--- ejercicios/4-DOM-pig-game/app.js
+++ ejercicios/4-DOM-pig-game/app.js
@@ -46,12 +46,30 @@ document.getElementById('current-1').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. Random number
     var dice = Math.floor((Math.random()*6) + 1);
 
+    // 2. Display result
     var diceDom = document.querySelector('.dice');
     diceDom.style.display = 'block';
     diceDom.src='dice-' + dice + '.png';
 
+    // 3. Update the round score if the rolled number was not a 1, else, next player
+    if (dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else{
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        document.querySelector('.player-0-panel').classList.toggle('active'); // Uso de toggle (IMPORTANTE)
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 
 
 }); // anonymous function... no se puede reutirlizar
