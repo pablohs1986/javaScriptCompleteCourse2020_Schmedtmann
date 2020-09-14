22e3ca5dbc212321bbd778b9a02d394c79d1f4f5
pablohs1986 <pabloherrero1986@gmail.com>
2020-09-14 23:28:30 +0200

    Til 79

--- ejercicios/6-budgety/app.js
+++ ejercicios/6-budgety/app.js
@@ -8,7 +8,27 @@ var budgetController = (function(){ // ---> Un módulo (IIFE + Closures)
 
 // UI CONTROLLER
 var UIController = (function() { // ---> Otro módulo
-    // Some code
+    
+var DOMstrings = { // Esto es privado
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+}
+
+    return {
+        getInput: function() {
+            return{ // --> Devuelve un objeto
+                type: document.querySelector(DOMstrings.inputType).value, // inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() { // Expone, publica las DOMstrings
+            return DOMstrings;
+        }
+    }
+
 })();
 
 
@@ -16,10 +36,13 @@ var UIController = (function() { // ---> Otro módulo
 // GLOBAL APP CONTROLLER
 var Controller = (function(budgetCtrl, UICtrl) { // ---> Otro módulo
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
 
         // 1. Get the filed input data
-
+        var input = UICtrl.getInput(); // --> Usa el método getInput de UIController
+        console.log(input);
         // 2. Add the item to de budget controller
 
         // 3. Add the new item to UI
@@ -30,7 +53,7 @@ var Controller = (function(budgetCtrl, UICtrl) { // ---> Otro módulo
         console.log('Works!');
     }
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) { // Esto es para que además de clickando el botón, también se ejecute el evento al pulsar enter (es la key nº 13)
 
